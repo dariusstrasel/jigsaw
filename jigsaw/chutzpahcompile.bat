e3fa1d6256fb95e4d86b8567311695efb05177dc
smonizmcmahon <smonizmcmahon@emoneyadvisor.com>
2019-03-21 16:53:13 -0400

    Wipe project for new incoming work.

--- jigsaw/chutzpahcompile.bat
+++
@@ -1,3 +0,0 @@
-﻿call npm install 
-call .\node_modules\.bin\babel.cmd --presets react,env js/*.jsx --out-file wwwroot/js/chutzpah.js
-call .\node_modules\.bin\babel.cmd --presets react,env jsx/testjsx/chutzpah.test.jsx --out-file jsx/testjs/chutzpah.test.js
