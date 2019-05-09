e40fe5e43d3f79ea54cdbba8bb331af6ab100308
Bernard Belanger <bbelanger@emoneyadvisor.com>
2019-05-09 17:04:18 -0400

    Add a draft controller, model, and repository for seats.

---
+++ Api/Controllers/SeatsController.cs
@@ -0,0 +1,35 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeatsController : ControllerBase
+    {
+        [HttpGet]
+        // GET api/seat
+        public ActionResult<IEnumerable<Seat>> Get()
+        {
+            var seats = new List<Seat>()
+            {
+                new Seat()
+                {
+                    Id = "1",
+                    Reservable = true
+                }
+            };
+            return seats;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Seat> Get(string id)
+        {
+            return new Seat() { Id = id };
+        }
+    }
+}
