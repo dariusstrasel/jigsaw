d1db6d1f3ad48f47ff3f4c2c39ce064a6efa79ce
smonizmcmahon <smonizmcmahon@emoneyadvisor.com>
2019-05-31 10:05:14 -0400

    Create Entity migration scripts for initial database setup.

---
+++ Api/Migrations/20190530205004_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Seats",
+                columns: table => new
+                {
+                    SeatId = table.Column<string>(nullable: false),
+                    Reservable = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PrimaryKey_SeatId", x => x.SeatId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Seats");
+        }
+    }
+}
